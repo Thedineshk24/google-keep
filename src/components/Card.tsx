f574a7323f47f4b05f613eb18a8706c0b773b44a
Dinesh Katariya <thedineshk24@gmail.com>
2022-07-27 12:04:49 +0530

    added delete msg

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,6 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 type Props = {
   data: {
     title: string;
@@ -10,10 +13,15 @@ type Props = {
 const Card = ({ data, id, deleteNote }: Props) => {
   const handleDelete = () => {
     deleteNote(id);
+    toast.success("Note deleted successfully", {
+      position: "top-right",
+      autoClose: 2000,
+    });
   };
 
   return (
       <div className="card lg:card-side w-60 bg-base-100 shadow-md mt-20">
+        <ToastContainer />
         <div className="card-body">
           <h2 className="card-title">{data.title}</h2>
           <p>{data.content}</p>
